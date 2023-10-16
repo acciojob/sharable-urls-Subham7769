32ab0a9d809db7e08cca94d8fc36b8c7ed0378c7
Subham jain <134865513+Subham7769@users.noreply.github.com>
2023-10-17 00:16:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+document.getElementById("button").addEventListener("click", function() {
+            const nameValue = document.getElementById("name").value;
+            const yearValue = document.getElementById("year").value;
+
+            // Build the query string
+            const queryString = `?name=${nameValue}&year=${yearValue}`;
+
+            // Update the URL text
+            document.getElementById("url").textContent = `URL: https://localhost:8080/${queryString}`;
+        });
